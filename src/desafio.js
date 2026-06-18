9d3498569d62a36a75f39a5f30760bfe49442bf2
EduardoCassin12 <eduardojuniorlopescassin@gmail.com>
2026-06-18 12:31:46 -0300

    Adiciona desafio classificador de nível do herói

--- src/desafio.js
+++ src/desafio.js
@@ -1,2 +1,22 @@
-console.log("Hello World")
-console.log("Hello")
+let name = "Geraldo da Massa"
+let xp =  5625
+
+if(xp <= 1000){
+    console.log(`O Herói ${name} está no nível Ferro`)
+} else if(xp <= 2000){
+    console.log(`O Herói ${name} está no nível Bronze`)
+} else if(xp <= 5000) {
+    console.log(`O Herói ${name} está no nível Prata`)
+}else if(xp <= 7000){
+         console.log(`O Herói ${name} está no nível Ouro`)
+} else if(xp <= 8000 ) {
+    console.log(`O Herói ${name} está no nível Platina`)
+} else if(xp <= 9000) {
+    console.log(`O Herói ${name} está no nível Ascendente`)
+}else if(xp <= 10000) {
+    console.log(`O Herói ${name} está no nível Imortal`)
+} else {
+    console.log(`O Herói ${name} está no nível Radiante`)
+}
+
+    
